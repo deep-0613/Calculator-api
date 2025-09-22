cf1be180bb45543c4e3939e6b37069f686a9a21f
Deep061007 <deepbandekar06@gmail.com>
2025-09-22 11:15:04 +0530

    updates

--- src/main/java/com/example/Calculator/CalculatorApplication.java
+++ src/main/java/com/example/Calculator/CalculatorApplication.java
@@ -1,4 +1,4 @@
-package com.example.Calculator;
+package com.example.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
